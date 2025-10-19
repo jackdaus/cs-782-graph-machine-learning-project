814425744969e0dc2bd9c12ed15ba47cb4f7a5e9
Jack Daus <jackdaus@gmail.com>
2025-10-19 14:04:39 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This repo explores usage of [pycolmap](https://colmap.github.io/pycolmap/index.html). 
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from colmap-experiments!")
+
+
+if __name__ == "__main__":
+    main()
