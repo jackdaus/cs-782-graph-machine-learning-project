e670d4ea33c6e113fc86eecd5fbd0768d11b767c
Jack Daus <jackdaus@gmail.com>
2025-12-06 23:25:05 -0500

    x28 large image features

--- utils/loaders/__init__.py
+++ utils/loaders/__init__.py
@@ -1,2 +1,8 @@
 from .loaders import load_reconstruction_to_graph, load_reconstruction_to_graph_sfm
-from .dataset import GraphPairDataset
+from .dataset import GraphPairDataset, SfmPairDataset, DataSfm
+from .efficient import (
+    ImageFeatureCache,
+    SfmSample,
+    EfficientSfmPairDataset,
+    compute_edge_index,
+)
